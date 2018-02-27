4330d78a6299b499842ae688b347043a49764c46
luchen01 <luchenpengnyc@gmail.com>
2018-02-26 23:46:01 -0800

    fix bugs

--- codegen.js
+++ codegen.js
@@ -16,9 +16,14 @@ const codegen = (ast) =>{
 
 const genNode = (node)=>{
   if(node.type === 'declaration'){
-    return 'let ' + node.identifier + '=' + node.name + ';\n'
+    return 'let ' + node.identifier + '=' + genNode(node.value) + ';\n'
+
+  } else if (node.type === 'assignment'){
+    return node.identifier + ' = ' + genNode(node.value);
+
   } else if (node.type === 'identifier'){
     return node.name;
+
   } else if (node.type === 'number'){
     return node.value;
 
@@ -26,18 +31,21 @@ const genNode = (node)=>{
     return genNode(node.left) + ' '
           + node.operator + ' '
           + genNode(node.right);
+
   } else if (node.type === 'if'){
     return 'if ('+
             genNode(node.condition) +
           ') {\n' +
             codegen(node.body) +
           '}';
+
   } else if(node.type === 'while'){
       return 'while ('+
               genNode(node.condition) +
             ') {\n' +
               codegen(node.body) +
             '}';
+
   } else if(node.type === 'comparison'){
 
       const operators = {
